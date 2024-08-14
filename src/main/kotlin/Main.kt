049a6df2952aba2577811a123ccd7a0690253f63
Alexey Kuzmin <alexey.kuzmin@quantori.com>
2024-08-14 22:27:57 +0200

    KS-1 added homework for Variables topic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    val totalSeconds = 6480
+    val hours = totalSeconds / 3600
+    val remainingSecondsAfterHours = totalSeconds % 3600
+
+    val minutes = remainingSecondsAfterHours / 60
+    val seconds = remainingSecondsAfterHours % 60
+
+    val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    println(formattedTime)
 }
