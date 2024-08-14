cccf6e75909a441ec654405fff3367f4c45775b2
Alexey Kuzmin <alexey.kuzmin@quantori.com>
2024-08-14 22:24:35 +0200

    KS-1 added homework for Variables topic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    var employeesNumber = 2000
+    //println(employeesNumber)
+
+    employeesNumber -= 1
+    println(employeesNumber)
 }
