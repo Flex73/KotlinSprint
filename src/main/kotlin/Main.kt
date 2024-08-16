463b6670f8d43223680b7a2e0e9066be0972bdee
Alexey Kuzmin <alexey.kuzmin@quantori.com>
2024-08-16 11:51:57 +0200

    KS-1-5 displayed formatted time in space and moved code to separate files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,23 @@
 package org.example
 
 fun main() {
-    getOrdersInfo()
-    getEmployeesNumber()
-    getFirstFlightStatistics()
+    val ordersNumber = 75
+    val gratitudeMessage = "Thanks for choosing us!"
+
+    println(ordersNumber)
+    println(gratitudeMessage)
+
+    val length = 40_868_600_000L
+    val age: Byte = 27
+    val partOfDay = 0.075f
+    val seconds: Short = 6480
+    val partOfYear = 2.0547945205479453E-4
+    val apogee = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
 }
--- src/main/kotlin/employees.kt
+++
@@ -1,9 +0,0 @@
-package org.example
-
-fun getEmployeesNumber() {
-    var employeesNumber = 2000
-    //println(employeesNumber)
-
-    employeesNumber -= 1
-    println(employeesNumber)
-}
