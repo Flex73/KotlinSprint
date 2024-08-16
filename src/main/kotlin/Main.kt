41ee78e261b826025ba8f09c5bfdc749188f924d
Alexey Kuzmin <alexey.kuzmin@quantori.com>
2024-08-16 12:13:43 +0200

    KS-1-1 added orders info

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,5 @@
 package org.example
 
 fun main() {
-    val ordersNumber = 75
-    val gratitudeMessage = "Thanks for choosing us!"
-
-    println(ordersNumber)
-    println(gratitudeMessage)
+    println("Hello world!")
 }
---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_1
+
+fun main() {
+    val ordersNumber = 75
+    val gratitudeMessage = "Thanks for choosing us!"
+
+    println(ordersNumber)
+    println(gratitudeMessage)
+}
