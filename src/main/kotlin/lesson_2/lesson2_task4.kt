3c9709d8b6840c5fb8ba1dd24467d62e0a60d206
Alexey Kuzmin <flex7398@gmail.com>
2024-08-18 00:38:37 +0200

    KS-2-4 used constant

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,12 +1,14 @@
 package org.example.lesson_2
 
+const val DIVIDER = 100
+
 fun main() {
     val crystalOreNumber = 7
     val ironOreNumber = 11
     val buffPercentage = 20
 
-    val bonusCrystalOre = (crystalOreNumber * buffPercentage) / 100
-    val bonusIronOre = (ironOreNumber * buffPercentage) / 100
+    val bonusCrystalOre = (crystalOreNumber * buffPercentage) / DIVIDER
+    val bonusIronOre = (ironOreNumber * buffPercentage) / DIVIDER
 
     println("Bonus Crystal Ore: $bonusCrystalOre")
     println("Bonus Iron Ore: $bonusIronOre")
