30e7bf3a524f0ca30bdc4d46dcf84355402b3151
Alexey Kuzmin <flex7398@gmail.com>
2024-08-18 18:00:45 +0200

    KS-3-4 output chess game data

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main() {
+    var turnFrom = "E2"
+    var turnTo = "E4"
+    var turnNumber = 1
+    println("$turnFrom:$turnTo,$turnNumber")
+
+    turnFrom = "D2"
+    turnTo = "D3"
+    turnNumber += 1
+    println("$turnFrom:$turnTo,$turnNumber")
+}
