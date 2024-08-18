2e537cfd124da44a984e379c79b90ebba8c13bd1
Alexey Kuzmin <flex7398@gmail.com>
2024-08-18 18:13:31 +0200

    KS-3-5 parsed turn data string and output it by parts

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -2,7 +2,6 @@ package org.example.lesson3
 
 fun main() {
     val turnData = "D2-D4;0"
-
     val turnDataParts = turnData.split(";")
     val (cellFrom, cellTo) = turnDataParts[0].split("-")
     val turnNumber = turnDataParts[1]
