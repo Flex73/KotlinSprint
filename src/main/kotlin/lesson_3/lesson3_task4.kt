4d19510e1df4cbcf8e0bb51e2d46645f18d2de89
Alexey Kuzmin <flex7398@gmail.com>
2024-08-18 21:56:58 +0200

    KS-3-4 fixed output format

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -4,10 +4,10 @@ fun main() {
     var turnFrom = "E2"
     var turnTo = "E4"
     var turnNumber = 1
-    println("[$turnFrom:$turnTo;$turnNumber]")
+    println("[$turnFrom-$turnTo;$turnNumber]")
 
     turnFrom = "D2"
     turnTo = "D3"
     turnNumber++
-    println("[$turnFrom:$turnTo;$turnNumber]")
+    println("[$turnFrom-$turnTo;$turnNumber]")
 }
