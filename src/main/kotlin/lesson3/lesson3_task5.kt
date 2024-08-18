02399f4efc18f00c82ec59cb4fa2e1272b633288
Alexey Kuzmin <flex7398@gmail.com>
2024-08-18 21:55:35 +0200

    KS-3-5 optimized destructuring

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -2,9 +2,7 @@ package org.example.lesson3
 
 fun main() {
     val turnData = "D2-D4;0"
-    val turnDataParts = turnData.split(";")
-    val (cellFrom, cellTo) = turnDataParts[0].split("-")
-    val turnNumber = turnDataParts[1]
+    val (cellFrom, cellTo, turnNumber) = turnData.split(";", "-")
 
     println(cellFrom)
     println(cellTo)
