ab3f8a459f0bda2756081770b3c5b219250a3bb5
Alexey Kuzmin <flex7398@gmail.com>
2024-08-20 09:56:07 +0200

    KS-4-3 added boolean constants

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -2,21 +2,21 @@ package org.example.lesson_4
 
 const val NOT_FAVORABLE_SEASON = "Winter"
 const val FAVORABLE_HUMIDITY = 20
-const val SUNNY_WEATHER = "Sunny"
-const val TENT_STATE_OPEN = "Open"
+const val IS_SUNNY_WEATHER = true
+const val IS_TENT_STATE_OPEN = true
 
 fun main() {
-    val weatherToday = "Sunny"
-    val tentState = "Open"
+    val isWeatherSunny = true
+    val isTentOpen = true
     val humidity = 20
     val currentSeason = "Winter"
 
-    val isWeatherSunny = SUNNY_WEATHER == weatherToday
-    val isTentOpen = tentState == TENT_STATE_OPEN
+    val isWeatherFavorable = IS_SUNNY_WEATHER == isWeatherSunny
+    val isOpen = isTentOpen == IS_TENT_STATE_OPEN
     val isHumidityFavorable = humidity == FAVORABLE_HUMIDITY
     val isSeasonFavorable = currentSeason != NOT_FAVORABLE_SEASON
-    val isConditionFavorable = isWeatherSunny
-            && isTentOpen
+    val isConditionFavorable = isWeatherFavorable
+            && isOpen
             && isHumidityFavorable
             && isSeasonFavorable
     println("Благоприятные ли условия сейчас для роста бобовых? $isConditionFavorable")
