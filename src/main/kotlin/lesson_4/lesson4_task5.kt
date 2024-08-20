f0235401b88a1e2c996e192f48c512184fd4cafc
Alexey Kuzmin <flex7398@gmail.com>
2024-08-20 10:06:25 +0200

    KS-4-5 added boolean constants

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -3,6 +3,8 @@ package org.example.lesson_4
 const val MIN_CREW_NUMBER = 55
 const val MAX_CREW_NUMBER = 70
 const val MIN_SUPPLY_NUMBER = 50
+const val IS_SHIP_DAMAGED = true
+const val IS_WEATHER_FAVORABLE = true
 
 fun main() {
     val isShipDamaged = readln().toBoolean()
@@ -10,10 +12,12 @@ fun main() {
     val supplyItemsAmount = readln().toInt()
     val isWeatherFavorable = readln().toBoolean()
 
+    val isShipReady = isShipDamaged != IS_SHIP_DAMAGED
+    val isWeatherSuitable = isWeatherFavorable == IS_WEATHER_FAVORABLE
     val isCrewNumberFit = crewNumber in MIN_CREW_NUMBER..MAX_CREW_NUMBER
     val isEnoughSupply = supplyItemsAmount > MIN_SUPPLY_NUMBER
     val isCrewFull = crewNumber == 70
-    val isShipReady = (!isShipDamaged && isCrewNumberFit && isEnoughSupply)
-            || (isCrewFull && isWeatherFavorable && isEnoughSupply)
-    println("The ship is ready: $isShipReady")
+    val isShipPrepared = (isShipReady && isCrewNumberFit && isEnoughSupply)
+            || (isCrewFull && isWeatherSuitable && isEnoughSupply)
+    println("The ship is ready: $isShipPrepared")
 }
